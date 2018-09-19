7e51b9848a0635ef5220d4b1c7c2c3e746090042
JeongHo <wwwkang8@gmail.com>
2018-09-20 01:25:09 +0900

    initial commit

---
+++ src/test/java/com/ajouict/inhousekitchen/InhousekitchenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ajouict.inhousekitchen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InhousekitchenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
