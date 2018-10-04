c984480ecda729c6b6061ee1136ef726851a0803
Jiwon Bae <jwb8705@gmail.com>
2018-10-04 12:28:00 +0900

    chore: [feature/menu-register] - Adds dependency to connect with S3 and project

---
+++ src/main/java/com/ajouict/inhousekitchen/util/S3Uploader.java
@@ -0,0 +1,10 @@
+package com.ajouict.inhousekitchen.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class S3Uploader {
+
+}
