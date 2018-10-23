6d8481ae42b2f3dc4f279fcd632a9b6bb837a755
박신명 <smsm91443@gamil.com>
2018-10-24 01:22:13 +0900

    review 리스트

--- src/main/java/com/ajouict/inhousekitchen/controller/ReviewController.java
+++ src/main/java/com/ajouict/inhousekitchen/controller/ReviewController.java
@@ -1,4 +1,26 @@
 package com.ajouict.inhousekitchen.controller;
 
+import com.ajouict.inhousekitchen.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/review")
 public class ReviewController {
+    @Autowired
+    private UserRepository userRepository;
+    /*
+    @GetMapping("/review")
+    public String create() {
+        return "/review/add_review";
+    }
+    */
+    @GetMapping("")
+    public String list(Model model){
+        model.addAttribute("users", userRepository.findAll());
+        return "/review/list_review";
+    }
 }
--- src/main/java/com/ajouict/inhousekitchen/domain/SearchRepository.java
+++ src/main/java/com/ajouict/inhousekitchen/domain/SearchRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SearchRepository extends JpaRepository<Host, Long> {
 
-
 }
