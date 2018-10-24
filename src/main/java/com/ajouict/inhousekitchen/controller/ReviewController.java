3c5cff0f7bed2d82e7589f6947764636b16cc7cb
박신명 <smsm91443@gamil.com>
2018-10-25 02:29:20 +0900

    REVIEW repository 생성

--- src/main/java/com/ajouict/inhousekitchen/controller/ReviewController.java
+++ src/main/java/com/ajouict/inhousekitchen/controller/ReviewController.java
@@ -7,17 +7,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
-//@RequestMapping("/review")
+@RequestMapping("/review")
 public class ReviewController {
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/{userId}")
-    public String create(@PathVariable String userId, Model model) {
+    public String create(@PathVariable String userId, Model model, HttpSession session) {
+        // 로그인하지 않았을 경우
+        if(!HttpSessionUtils.isLoginUser(session)){
 
+        }
+        User loginUser = HttpSessionUtils.getUserFromSession(session);
+
+        model.addAttribute("loginUser", loginUser);
         model.addAttribute("user", userRepository.findByUserId(userId));
         return "/review/add_review";
     }
@@ -26,5 +35,10 @@ public class ReviewController {
     public String list(Model model){
         model.addAttribute("users", userRepository.findAll());
         return "/review/list_review";
+    }
+
+    @PutMapping("/create")
+    public String create(HttpSession session){
+        return "redirect:/review/list";
     }
 }
--- src/main/java/com/ajouict/inhousekitchen/domain/Review.java
+++ src/main/java/com/ajouict/inhousekitchen/domain/Review.java
@@ -1,4 +1,32 @@
 package com.ajouict.inhousekitchen.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_review_writer"))
+    private User writer;
+
+    private String title;
+
+    @Lob
+    private String contents;
+
+    private LocalDateTime createDate;
+
+    public Review() {};
+
+    public Review(User writer, String title, String contents){
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createDate = LocalDateTime.now();
+    }
+
 }
