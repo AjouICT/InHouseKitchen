a6cbf321951efe8f05fb5c5cfee70517ea8fc8da
JeongHo <wwwkang8@gmail.com>
2018-11-17 21:17:03 +0900

    feat:[feature/develop <Back-End> - Changed variables naming format to CamelCase]

--- src/main/java/com/ajouict/inhousekitchen/domain/Host.java
+++ src/main/java/com/ajouict/inhousekitchen/domain/Host.java
@@ -2,36 +2,40 @@ package com.ajouict.inhousekitchen.domain;
 
 import com.ajouict.inhousekitchen.dto.HostDto;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 public class Host {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
     private String introduction;
 
     @NotNull
-    private String contact_info;
+    private String contactInfo;
 
     @Column(nullable = true)
-    private int max_guest;
+    private int maxGuest;
 
     @Embedded
     @AttributeOverrides({
             @AttributeOverride(name="latitude",
                     column=@Column(name="LOCATION_LATITUDE")),
             @AttributeOverride(name="longitude",
-                    column=@Column(name="LOCATION_LOGITUDE"))
+                    column=@Column(name="LOCATION_LONGITUDE"))
     })
     private Location location;
 
@@ -62,10 +66,13 @@ public class Host {
     private User myself;
 
     @Column(nullable = true)
-    private double avg_score;
+    private double avgScore;
 
     @Column(nullable = true)
-    private int total_guest_number;
+    private int totalGuestNumber=0;
+
+    private int totalScore=0;
+
 
     @Column(nullable = true)
     @ElementCollection
@@ -75,14 +82,19 @@ public class Host {
     )
     private Set<MenuImage> menuImages = new HashSet<>();
 
+    @OneToMany(mappedBy = "host")
+    private List<Review> reviews;
+
+
+
     private String availablePeriod;
 
     public Host(){}
 
     @Builder
-    public Host(String introduction, @NotNull String contact_info,  Location location, MenusInfo menusInfo, String availablePeriod) {
+    public Host(String introduction, @NotNull String contactInfo,  Location location, MenusInfo menusInfo, String availablePeriod) {
         this.introduction = introduction;
-        this.contact_info = contact_info;
+        this.contactInfo = contactInfo;
         this.location = location;
         this.menusInfo = menusInfo;
         this.availablePeriod = availablePeriod;
@@ -102,7 +114,7 @@ public class Host {
     public HostDto _toHostDto() {
         return HostDto.builder()
                 .introduction(this.introduction)
-                .contact_info(this.contact_info)
+                .contact_info(this.contactInfo)
                 .menu_name(this.menusInfo.getName())
                 .menu_price(Double.toString(this.menusInfo.getPrice()))
                 .menu_description(this.menusInfo.getDescription())
@@ -122,6 +134,39 @@ public class Host {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public void calculateScore(int score, int flag){
+
+        // 추가
+        if (flag == 1){
+            this.totalScore += score; // 점수 증가
+            this.totalGuestNumber += 1; // 사람 증가
+        }
+        // 수정
+        else if (flag == 2){
+            this.totalScore += score;
+        }
+        // 삭제
+        else{
+            System.out.println("삭제 전 인원 수 : " + this.totalGuestNumber);
+            System.out.println("삭제 전 평점 수 : " + this.totalScore);
+            this.totalScore -= score;
+            this.totalGuestNumber--;
+            System.out.println(" 인원 수 : " + this.totalGuestNumber);
+            System.out.println(" 평점 수 : " + this.totalScore);
+        }
+        this.avgScore = (float)this.totalScore / this.totalGuestNumber;
+        // 소수점 첫째자리까지만 저장
+        this.avgScore = (int)(this.avgScore * 10);
+        this.avgScore = this.avgScore / 10;
+    }
+
+    public boolean IsSameHost(User loginUser){
+        if(loginUser == null){
+            return false;
+        }
+        return this.id.equals(loginUser.getId());
     }
 
 }
